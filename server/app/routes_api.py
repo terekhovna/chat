3681d30c69dd13dcaa8e752520ca07967df772aa
Nikolay Terekhov <terekhov.na@phystech.edu>
2019-05-16 18:32:11 +0300

    add exeception support

--- server/app/routes_api.py
+++ server/app/routes_api.py
@@ -2,9 +2,20 @@ from app import app
 from flask import jsonify, request
 from app.models import User, Chat
 from flask import make_response
+from werkzeug import exceptions
 from app.authentic import auth
 from app.api_logic import get_user, get_max_id, register_user,\
     to_json, create_chat, delete_chat, post_message, get_messages
+
+
+def get_json():
+    try:
+        js = request.get_json(force=True)
+    except exceptions.BadRequest:
+        return None
+    if not isinstance(js, dict):
+        return None
+    return js
 
 
 def return_error(s: str = ""):
@@ -29,7 +40,10 @@ def api_login():
 
 @app.route('/api/register/', methods=['POST'])
 def api_register():
-    js = request.get_json(force=True)
+    js = get_json()
+
+    if not js:
+        return return_error('corrupted json')
 
     if not js.get('username'):
         return return_error('i need username')
@@ -58,7 +72,10 @@ def api_friend_get():
 @app.route('/api/friend/', methods=['POST'])
 @auth.login_required
 def api_friend_post():
-    js = request.get_json(force=True)
+    js = get_json()
+
+    if not js:
+        return return_error('corrupted json')
 
     if not js.get('friend_name'):
         return return_error('i need friend name')
@@ -78,7 +95,10 @@ def api_friend_post():
 @app.route('/api/friend/', methods=['DELETE'])
 @auth.login_required
 def api_friend_delete():
-    js = request.get_json(force=True)
+    js = get_json()
+
+    if not js:
+        return return_error('corrupted json')
 
     if not js.get('friend_name'):
         return return_error('i need friend name')
@@ -106,8 +126,12 @@ def api_get_chats():
 @app.route('/api/chats/', methods=['POST'])
 @auth.login_required
 def api_create_chat():
+    js = get_json()
+
+    if not js:
+        return return_error('corrupted json')
+
     user = get_user(username=auth.username())
-    js = request.get_json(force=True)
     if not js.get('id'):
         js['id'] = get_max_id(Chat) + 1
     if not js.get('title'):
@@ -122,8 +146,12 @@ def api_create_chat():
 @app.route('/api/chats/', methods=['DELETE'])
 @auth.login_required
 def api_delete_chat_for_user():
+    js = get_json()
+
+    if not js:
+        return return_error('corrupted json')
+
     user = get_user(username=auth.username())
-    js = request.get_json(force=True)
     if not js.get('id'):
         return return_error("i need id of chat")
     re = delete_chat(user, js['id'])
@@ -148,8 +176,12 @@ def api_get_messages(chat_id):
 @app.route('/api/chats/<int:chat_id>/', methods=['POST'])
 @auth.login_required
 def api_post_message(chat_id):
+    js = get_json()
+
+    if not js:
+        return return_error('corrupted json')
+
     user = get_user(username=auth.username())
-    js = request.get_json(force=True)
 
     if not js.get('text'):
         return return_error('i need text of message')
